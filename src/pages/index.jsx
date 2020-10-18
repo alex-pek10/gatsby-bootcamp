df4ac85e3628341233363cf05c2afb97755408c5
Alex Myers <alex@pek10.dev>
2020-10-18 19:23:47 -0400

    Reafactor for JSX

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>Hello!</h1>
+      <p>I'm Alex, a full-stack developer living in Columbus, Ohio.</p>
+    </>
+  )
+}
+
+export default HomePage
