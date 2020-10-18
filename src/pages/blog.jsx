34a25b110534a4d6dc61a61d48fd7e5ec8536edd
Alex Myers <alex@pek10.dev>
2020-10-18 19:31:14 -0400

    Create 3 inner pages

---
+++ src/pages/blog.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const BlogPage = () => {
+  return (
+    <>
+      <h1>Blog</h1>
+      <p>A list of posts will be here.</p>
+    </>
+  )
+}
+
+export default BlogPage
---
+++ src/pages/contact.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <>
+      <h1>Contact Me</h1>
+      <p>Here's how you can reach me...</p>
+    </>
+  )
+}
+
+export default ContactPage
