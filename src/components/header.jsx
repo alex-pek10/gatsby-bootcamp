459cdb1fa100564de4549362759df73e5fa1f031
Alex Myers <alex@pek10.dev>
2020-10-18 20:02:43 -0400

    Add: Navbar to Header and import

---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Navbar from "./navbar"
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar />
+
+      <h1>Header</h1>
+    </header>
+  )
+}
+
+export default Header
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,28 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
 
+import Header from "../components/header"
 import Footer from "../components/footer"
 
 const HomePage = () => {
   return (
     <>
-      {/* NAVBAR */}
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about/">About</Link>
-          </li>
-          <li>
-            <Link to="/blog/">Blog</Link>
-          </li>
-          <li>
-            <Link to="/contact/">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+      <Header />
 
       {/* MAIN CONTENT */}
       <main>
