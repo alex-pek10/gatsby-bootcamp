9d54c2c749a4beb069d21d07aa02b799d58d5efe
Alex Myers <alex@pek10.dev>
2020-10-18 20:59:31 -0400

    Update: Convert Navbar to Bulma style

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,21 +3,45 @@ import { Link } from "gatsby"
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about/">About</Link>
-        </li>
-        <li>
-          <Link to="/blog/">Blog</Link>
-        </li>
-        <li>
-          <Link to="/contact/">Contact</Link>
-        </li>
-      </ul>
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="container">
+        <div className="navbar-brand">
+          <Link className="navbar-item" to="/">
+            Gatsby Bootcamp
+          </Link>
+
+          <button
+            className="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarMain"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </button>
+        </div>
+
+        <div id="navbarMain" className="navbar-menu">
+          <div className="navbar-end">
+            <Link className="navbar-item" to="/">
+              Home
+            </Link>
+
+            <Link className="navbar-item" to="/about/">
+              About
+            </Link>
+
+            <Link className="navbar-item" to="/blog/">
+              Blog
+            </Link>
+
+            <Link className="navbar-item" to="/contact/">
+              Contact
+            </Link>
+          </div>
+        </div>
+      </div>
     </nav>
   )
 }
